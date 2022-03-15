d2466a63abd1c5a4c0339a7c17db845ccac80120
Jennivis <n1vije00@students.oamk.fi>
2022-03-15 13:12:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 import './App.css';
 
+const URL = 'http://localhost/shoppinglist/';
+
 function App() {
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setItems(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }, []);
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:item});
+    axios.post(URL + 'add.php',json, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then ((response) => {
+      setItems(items => [...items,response.data]);
+      setItem('');
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }
+
+  function remove(id) {
+    const json = JSON.stringify({id:id})
+    axios.post(URL + 'delete.php',json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+      .then((response) => {
+        const newListWithoutRemoved = items.filter((item) => item.id !== id);
+        setItems(newListWithoutRemoved);
+      }).catch (error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={save}>
+        <label>New item</label>
+        <input value={item} placeholder='Add a new item' onChange={e => setItem(e.target.value)}/>
+        <button>Save</button>
+      </form>
+      <ol>
+        {items?.map(item => (
+          <li key={item.id}>
+            {item.description}
+            <a className='delete' onClick={() => remove(item.id)} href="#"> Delete</a>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
